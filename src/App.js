99cf0cb6bbdb14cb59c42a2999691daeddc57996
NeuroResist <zeit7@rambler.ru>
2024-10-18 21:17:20 +0300

    fix: Исправлены баги

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './pages/Home';
-import './App.css'
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -12,10 +12,8 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-
         </Routes>
         <Footer />
-
       </div>
     </Router>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,36 @@
-import "./Header.css"
-// import "./Header_mobile.css"
-import Autocomplete from "./Autocomplete"
-import { v4 as uuidv4 } from 'uuid';
-import React, { useState, useEffect } from 'react';
-import Cart from "../components/Cart"
+import "./Header.css";
+import Autocomplete from "./Autocomplete";
+import { v4 as uuidv4 } from "uuid";
+import React, { useState, useEffect } from "react";
+import Cart from "../components/Cart";
 
 const useCartListener = () => {
   const [cart, setCart] = useState(() => {
     // Получаем корзину из localStorage при инициализации
-    const savedCart = localStorage.getItem('cart');
+    const savedCart = localStorage.getItem("cart");
     return savedCart ? JSON.parse(savedCart) : [];
   });
 
   // Функция для обновления состояния корзины
   const updateCart = () => {
-    const updatedCart = localStorage.getItem('cart');
+    const updatedCart = localStorage.getItem("cart");
     if (updatedCart) {
       setCart(JSON.parse(updatedCart));
     } else {
       setCart([]);
     }
-
   };
 
   useEffect(() => {
     // Отслеживание изменений localStorage в разных вкладках
     const handleStorageChange = (event) => {
-      if (event.key === 'cart') {
+      if (event.key === "cart") {
         updateCart();
       }
     };
 
     // Добавляем слушатель события 'storage'
-    window.addEventListener('storage', handleStorageChange);
+    window.addEventListener("storage", handleStorageChange);
 
     // Время от времени проверяем обновление корзины в текущей вкладке
     const interval = setInterval(() => {
@@ -41,7 +39,7 @@ const useCartListener = () => {
 
     // Убираем слушатель при размонтировании компонента
     return () => {
-      window.removeEventListener('storage', handleStorageChange);
+      window.removeEventListener("storage", handleStorageChange);
       clearInterval(interval);
     };
   }, []);
@@ -55,52 +53,77 @@ function Header() {
   // console.log(totalCart);
   const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
   console.log(isMobile ? "Мобильное" : "Не мобильное");
-  const suggestions = ['Молоко', 'Яблоки', 'Orange', 'Pineapple', 'Grapes', 'HTML', "CSS", "JavaScript"];
+  const suggestions = [
+    "Молоко",
+    "Яблоки",
+    "Orange",
+    "Pineapple",
+    "Grapes",
+    "HTML",
+    "CSS",
+    "JavaScript",
+  ];
 
   const setCartEnable = () => {
-      setCartEnableState(!cartEnable);
+    setCartEnableState(!cartEnable);
   };
 
-  return (  
+  return (
     <nav>
       <header className="header">
-          <div className="header__logo">
-            <img 
-            src={isMobile ? "../images/logo49.png" : "../images/logo152.png"}  
-            className={isMobile ? "mobile" : ""} 
-            onClick={() => window.location.href = "/"}
-            alt="Лого"/>
-          </div>
-          
-          <button  className={isMobile ? "header__catalog-button" : "header__catalog-button mobile"}>
-            <img src="../images/icons/menu_icon_white.png" alt=""/>
-            {isMobile ? "" : "Каталог"}
-          </button>
-          <div className={isMobile ? "search-bar" : "search-bar mobile"}>
-            <Autocomplete suggestions={suggestions}/>
+        <div className="header__logo">
+          <img
+            src={isMobile ? "../images/logo49.png" : "../images/logo152.png"}
+            className={isMobile ? "mobile" : ""}
+            onClick={() => (window.location.href = "/")}
+            alt="Лого"
+          />
+        </div>
 
+        <button
+          className={
+            isMobile
+              ? "header__catalog-button"
+              : "header__catalog-button mobile"
+          }
+        >
+          <img src="../images/icons/menu_icon_white.png" alt="" />
+          {isMobile ? "" : "Каталог"}
+        </button>
+        <div className={isMobile ? "search-bar" : "search-bar mobile"}>
+          <Autocomplete suggestions={suggestions} />
+        </div>
+        <div className={isMobile ? "header-icons" : "header-icons mobile"}>
+          <div className="favorites">
+            <img src="../images/icons/favorite_icon24.png" alt="Избранное" />
+            <span>Избранное</span>
           </div>
-          <div className={isMobile ? "header-icons" : "header-icons mobile"}>
-              <div className="favorites">
-                  <img src="../images/icons/favorite_icon24.png" alt="Избранное"/>
-                  <span>Избранное</span>
-              </div>
-              <div className="orders">
-                  <img src="../images/icons/orders-icon24.png" alt="Заказы"/>
-                  <span>Заказы</span>
-              </div>
-              <div className="cart" onClick={setCartEnable}>
-                  <img src="../images/icons/shop-cart-icon24.png" alt="Корзина"/>
-                  <span>Корзина</span>
-                  <div className="notification"><p>{ totalCart ? totalCart : "0"}</p></div>
-              </div>
-              <div className={isMobile ? "user-profile" : "user-profile mobile"}>
-                  <img src="../images/icons/user-avatar.png" className='avatar' alt="Аватар"/>
-                  <p>Алексей</p>
-                  <img src="../images/icons/schevron_down.png" className="arrow-down" alt="Стрелка"/>
-              </div>
+          <div className="orders">
+            <img src="../images/icons/orders-icon24.png" alt="Заказы" />
+            <span>Заказы</span>
           </div>
-        </header>
+          <div className="cart" onClick={setCartEnable}>
+            <img src="../images/icons/shop-cart-icon24.png" alt="Корзина" />
+            <span>Корзина</span>
+            <div className="notification">
+              <p>{totalCart ? totalCart : "0"}</p>
+            </div>
+          </div>
+          <div className={isMobile ? "user-profile" : "user-profile mobile"}>
+            <img
+              src="../images/icons/user-avatar.png"
+              className="avatar"
+              alt="Аватар"
+            />
+            <p>Алексей</p>
+            <img
+              src="../images/icons/schevron_down.png"
+              className="arrow-down"
+              alt="Стрелка"
+            />
+          </div>
+        </div>
+      </header>
     </nav>
   );
 }
