1b528ac4f6d609c6ca6b189a73515fcf1505b9c1
amnesiawho <92972232+AmnesiaXxXxX@users.noreply.github.com>
2024-10-21 16:27:59 +0000

    feat: Добавил функционал строке поиска

--- src/components/Header/Autocomplete/Autocomplete.tsx
+++ src/components/Header/Autocomplete/Autocomplete.tsx
@@ -1,36 +1,100 @@
-import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+import React from 'react';
+import "./Autocomplete.css"
 
-
-function Autocomplete({ placeholder }: { placeholder: string }) {
-    const items = [
-        {
-          id: 0,
-          name: 'Cobol'
-        },
-        {
-          id: 1,
-          name: 'JavaScript'
-        },
-        {
-          id: 2,
-          name: 'Basic'
-        },
-        {
-          id: 3,
-          name: 'PHP'
-        },
-        {
-          id: 4,
-          name: 'Java'
-        }
-      ]
-    return (
-        <ReactSearchAutocomplete
-            items={items}
-            placeholder={placeholder}
-            autoFocus
-        />
-    );
+interface AutocompleteProps {
+  suggestions: string[];
 }
 
+const Autocomplete: React.FC<AutocompleteProps> = ({ suggestions }) => {
+  const [filteredSuggestions, setFilteredSuggestions] = React.useState<string[]>([]);
+  const [inputValue, setInputValue] = React.useState<string>('');
+  const [inputFocused, setInputFocused] = React.useState<boolean>(false);
+  const [highlightedIndex, setHighlightedIndex] = React.useState<number>(-1);
+
+  const handleSend = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      console.log(' ', inputValue);
+      setInputValue('');
+    }
+  };
+
+  const handleFocus = () => {
+    setInputFocused(true);
+  };
+
+  const handleBlur = () => {
+    setTimeout(() => {
+      setInputFocused(false);
+    }, 100);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setInputValue(value);
+
+    const filtered = suggestions.filter(suggestion =>
+      suggestion.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredSuggestions(filtered);
+
+    if (filtered.length === 0) {
+      setFilteredSuggestions(['Результатов по вашему запросу нет!']);
+    }
+
+    setHighlightedIndex(-1);
+  };
+
+  const handleSelect = (value: string) => {
+    setInputValue(value);
+    setFilteredSuggestions([]);
+    setHighlightedIndex(-1);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'ArrowDown') {
+      setHighlightedIndex(prevIndex =>
+        Math.min(prevIndex + 1, filteredSuggestions.length - 1)
+      );
+    } else if (event.key === 'ArrowUp') {
+      setHighlightedIndex(prevIndex =>
+        Math.max(prevIndex - 1, 0)
+      );
+    } else if (event.key === 'Enter') {
+      if (highlightedIndex >= 0) {
+        handleSelect(filteredSuggestions[highlightedIndex]);
+      }
+    }
+  };
+
+  return (
+    <div className="autocomplete">
+      <input
+        type="text"
+        className={`search-bar ${filteredSuggestions.length > 0 ? 'with-suggestions' : ''}`}
+        value={inputValue}
+        onChange={handleChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onKeyDown={handleKeyDown}
+        placeholder="Search..."
+      />
+      {inputFocused && filteredSuggestions.length > 0 && (
+        <ul className={filteredSuggestions.length < 1 ? "suggestions-list empty" : "suggestions-list"}>
+          {filteredSuggestions.map((suggestion, index) => (
+            <li
+              key={suggestion}
+              className={index === highlightedIndex ? 'highlighted' : ''}
+              onClick={() => handleSelect(suggestion)}
+              onMouseOver={() => setHighlightedIndex(index)}
+            >
+              {suggestion}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
 export default Autocomplete;
+
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,15 +6,67 @@ export interface HeaderProps {
 }
 
 export const Header: React.FC<HeaderProps> = () => {
+    const products = [
+        {
+          id: 1,
+          count: 1,
+          name: 'Г/Ц Блинчики с мясом вес, Россия',
+          country: 'Россия',
+          oldPrice: 50.50,
+          newPrice: 44.50,
+          discount: 10,
+          imageUrl: '/images/products/blinchiki.png',
+          rating: 2,
+        },
+        {
+          id: 2,
+          count: 1,
+          name: 'Молоко ПРОСТОКВАШИНО Паст. питьевое цельное отборное...',
+          oldPrice: 50.50,
+          newPrice: 44.50,
+          discount: 10,
+          imageUrl: '/images/products/moloko.png',
+          rating: 3,
+        },
+        {
+          id: 3,
+          count: 1,
+          name: 'Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...',
+          description: 'МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...',
+          oldPrice: 50.50,
+          newPrice: 44.50,
+          discount: 10,
+          imageUrl: '/images/products/kalbasa.png',
+          rating: 5,
+        },
+        {
+          id: 4,
+          count: 1,
+          name: 'Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...',
+          description: 'МЯСНАЯ ИСТОРИЯ Молочные и С сыром...',
+          oldPrice: 50.50,
+          newPrice: 44.50,
+          discount: 10,
+          imageUrl: '/images/products/sosiski.png',
+          rating: 4,
+        },
+      ];
+      const suggestion = [
+        'JavaScript', 'TypeScript', 'React', 'Redux', 'NodeJS', 'Express', 'MongoDB',
+        'HTML', 'CSS', 'Sass', 'LESS', 'Angular', 'Vue', 'Next.js', 'Gatsby',
+        'Webpack', 'Babel', 'Jest', 'Mocha', 'Chai', 'Cypress', 'Jasmine',
+        'GraphQL', 'Apollo', 'Relay', 'Docker', 'Kubernetes', 'AWS', 'Azure',
+        'Firebase', 'Heroku', 'Netlify', 'Git', 'GitHub', 'GitLab', 'Bitbucket',
+        'Jenkins', 'CircleCI', 'TravisCI', 'Yarn', 'npm', 'pnpm', 'D3.js', 'Chart.js',
+        'Lodash', 'Moment.js', 'Ramda', 'RxJS', 'MobX', 'Redux Saga'
+      ];
     return (
         <header className="header">
             <div className="header__content">
                 <img className='logo' src="images/logo152.png"></img>
                 
                 <Link className='header__content__catalog' to="/"><img src='images/icons/menu-icon-white.png'></img>Каталог</Link>
-                <div className="header__content__search-bar">
-                    <Autocomplete placeholder="Найти товар"/>
-                </div>
+                <Autocomplete suggestions={suggestion} />
                 <div className="header__content__user-space">
                     <Link to="/register">Избранное</Link>
                     <Link to="/login">Заказы</Link>
