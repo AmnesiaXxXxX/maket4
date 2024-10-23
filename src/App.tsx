7acac43c5b48da5506ddc46345f0444c805bf9a6
amnesiawho <92972232+AmnesiaXxXxX@users.noreply.github.com>
2024-10-23 09:40:25 +0000

    refactor: Добавление страницы 404

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/Header/Header';
 import Home from './pages/Home/Home';
-
+import Cart from './pages/Cart/Cart';
+import NotFound from './pages/NotFound/NotFound';
 function App() {
   return (
     <BrowserRouter>
@@ -11,7 +12,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Home />} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path='*' element={<NotFound/>}/>
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound: React.FC = () => {
+    return (
+        <div>
+            <h1>Страница не найдена</h1>
+            <p>Извините, запрашиваемая страница не найдена.</p>
+        </div>
+    );
+};
+
+export default NotFound;
